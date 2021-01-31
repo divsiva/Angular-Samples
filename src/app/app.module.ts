39de6c12ad4bfa0b156def82797ed5c178f1f9b4
divsiva <divyajs.26@gmail.com>
2021-01-31 16:07:15 +0530

    Done naming correction in the Gallery componenet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GalleryComponent } from './gallery/gallery.component';
+import { GalleryComponent } from './Gallery/gallery.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
